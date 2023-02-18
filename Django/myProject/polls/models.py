b7aa85b9de95183ea135483d9d8565017f1115df
Shin <sinyubi04@gmail.com>
2023-02-19 08:50:19 +0900

    Django Backend DB update

--- Django/myProject/polls/models.py
+++ Django/myProject/polls/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Owner(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  phone = models.CharField(max_length=30)
+  def __str__(self):
+    return self.first_name + " " + self.last_name
+
+class Patient(models.Model):
+  breed = models.CharField(max_length=200)
+  pet_name = models.CharField(max_length=200)
+  age = models.IntegerField(default=0)
+  #One to many relationship Owner-Patient
+  owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+
+  DOG = "DO"
+  CAT = "CA"
+  BIRD = "BI"
+  REPTILE = "RE"
+  OTHER = "OT"
+  ANIMAL_TYPE_CHOICES = [
+  (DOG, "Dog"),
+  (CAT, "Cat"),
+  (BIRD, "Bird"),
+  (REPTILE, "Reptile"),
+  (OTHER, "Other"),
+  ]
+  animal_type = models.CharField(max_length=2, choices=ANIMAL_TYPE_CHOICES, default=OTHER)
+
+  class Meta:
+    ordering = ['pet_name'] #Order by pet_name
+  def __str__(self):
+    return self.pet_name + ', ' + self.breed
